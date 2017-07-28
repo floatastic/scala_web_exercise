11729e804d5f908b14431c04f6305bc8a84aa7e4
Michal Kreft <yomajkel@gmail.com>
2017-07-28 23:25:57 +0200

    add basic react scripts

---
+++ ui/scripts/sun-weather-component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class SunWeatherComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sunrise: null,
+            sunset: null,
+            temperature: null,
+            requests: null
+        };
+    }
+
+    render = () => {
+        return <div>
+            <h1>San Sebastian, Spain</h1>
+            <div>Sunrise time: {this.state.sunrise}</div>
+            <div>Sunset time: {this.state.sunset}</div>
+            <div>Current temperature: {this.state.temperature}</div>
+            <div>Requests: {this.state.requests}</div>
+        </div>
+    }
+}
+
+export default SunWeatherComponent;
